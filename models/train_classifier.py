1ef9abef34e8594eb8b07bcfc7ec9d1be779db7c
mustafaabdelaziz <mustafamabdelaziz@gmail.com>
2023-06-23 10:53:17 +0300

    added the classifier.pkl and modified training model

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,124 @@
 import sys
+# import libraries
+import pandas as pd
+from sqlalchemy import create_engine
+import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('stopwords')
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+import re
+
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.neighbors import KNeighborsClassifier
+import pickle
+
 
 
 def load_data(database_filepath):
-    pass
+    """ Loading data from SQL Database
 
+    Args:
+        database_filepath: The file path for the SQL DataBase.
+
+    Returns:
+        X: A dataFrame contains The messages.
+        Y: A DataFrame contains the Categories.
+        category_names: A list of Category names.
+    """
+    engine = create_engine(f"sqlite:///{database_filepath}")
+    df = pd.read_sql_table("DisasterResponse",engine)
+    X = df['message']
+    Y = df.iloc[:,4:]
+    category_names = df.columns[3:36]
+    return X, Y, category_names
 
 def tokenize(text):
-    pass
+    """Transforming the message Text into a machine learning usable form.
+    
+    Args: 
+        text: The message sent by the user.
+
+    Returns:
+        lemmated: Transformed and cleaned list of words. 
+    """
+    #Normalization
+    text = re.sub(r"\W", " ",text.lower())
+    
+    #Tokenization
+    tokens = word_tokenize(text)
+    
+    #Lemmatization and stop words removel
+    lemmatizer = WordNetLemmatizer()
+    lemmated = [lemmatizer.lemmatize(w).strip() for w in tokens if w not in stopwords.words('english')]
+    
+    return lemmated
 
 
 def build_model():
-    pass
+    """Building the Machine learning Pipeline to predict the categories of text.
+
+    Args:
+        None
+    
+    Returns:
+        cv: Optimized machine learning pipeline.
+    """
+    pipeline = Pipeline([ 
+    ("vect", CountVectorizer(tokenizer=tokenize)),
+    ("tf-idf", TfidfTransformer()),
+    ("clf",KNeighborsClassifier())
+    ])
+    parameters = {
+#         'vect__ngram_range': ((1, 1), (1, 2)),
+        'clf__n_neighbors': [5,10, 15,30,50 ], # the optemal n_neighbors number is 10
+        'clf__leaf_size': [20, 30, 50] # the optemal leaf_size is 20
+    }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+    return cv
+
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    """Evaluating the Machine Learning model.
+
+    Args:
+        model: The machine learning model used to classefy text categories.
+        X_test: Messages used as Testing Dataset.
+        Y_test: The real categories of the testing messages.
+        category_names: The names of the categories.
+
+    Returns:
+        None
+    """
+    y_pred = model.predict(X_test)
+    labels = category_names
+    
+    for i in range(36):
+        print(Y_test.columns[i], ':')
+        print(classification_report(Y_test.iloc[:,i], y_pred[:,i]))
+    
 
 
 def save_model(model, model_filepath):
-    pass
+    """Saving Classification model.
+
+    Args:
+        model: The machine learning model used to classefy text categories.
+        model_filepath: The file bath to save the model at.
+
+    Returns:
+        None.
+    """
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
